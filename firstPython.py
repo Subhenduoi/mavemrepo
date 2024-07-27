6d4096bd2f9794813a672f4f3d59b29c461ddf19
Subhendu <Subhenduoi@yahoo.co.in>
2024-07-27 21:06:53 +0530

    Python HTTP Client

--- firstPython.py
+++ firstPython.py
@@ -1,2 +1,19 @@
-print("Hellow World")
-print("Webhook -16 ")
+import http.client
+import json
+
+print("We are trying to execute an API call through Resquest Cleint")
+# The API endpoint
+url = "fakestoreapi.com"
+connection = http.client.HTTPSConnection(url)
+# A GET request to the API
+connection.request("GET", "/products/1")
+response = connection.getresponse()
+encoding = response.info().get_content_charset('utf8')
+# Print the response
+print("Status: {} and reason: {}".format(response.status, response.reason))
+if(response.status == 200 and response.reason == 'OK'):
+    responseObject =json.loads(response.read().decode(encoding))
+    print("Title: {}".format(responseObject['title']))
+else:
+    print('Issue in calling the API')
+connection.close()
