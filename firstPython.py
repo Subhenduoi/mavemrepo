5dfa90ac242dcb1570a933f0bb218bb867168005
Subhendu <Subhenduoi@yahoo.co.in>
2024-07-27 23:07:48 +0530

    Python HTTP Client

--- firstPython.py
+++ firstPython.py
@@ -24,16 +24,22 @@ if(response.status == 200 and response.reason == 'OK'):
     json_post_data = json.dumps(json.load(dataFile))
     dataFile.close()
     print(json_post_data)
-    
+
     #Posting the Content to the API
     headers = {'Content-type': 'application/json'}
     connection.request('POST', '/products', json_post_data, headers)
     response = connection.getresponse()
     
     print("Status: {} and reason: {}".format(response.status, response.reason))
+    
     encoding = response.info().get_content_charset('utf8')
     responseObject=json.loads(response.read().decode(encoding))
     print(responseObject)
+    if(response.status == 200 and response.reason == 'OK'):
+        with open(os.path.join(os.path.dirname(__file__), 'response.json'), 'w', encoding='utf-8') as f:
+            json.dump(responseObject, f, ensure_ascii=False, indent=4)
+    else:
+        print('Issue in calling the POST API')
 else:
-    print('Issue in calling the API')
+    print('Issue in calling the GET API')
 connection.close()
